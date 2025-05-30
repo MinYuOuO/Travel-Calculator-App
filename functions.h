48538327a1ec96b645290498c320c9c0cb56161e
Min Yu <109456449+MinYuOuO@users.noreply.github.com>
2025-05-30 09:11:21 +0800

    Split Functions into Modular Source Files
    refactor(*): split core logic into separate source files
    
    modularize project by extracting logic from a monolithic file into:
    - main.cpp: entry point and high-level flow
    - travel-calculator-app.cpp: travel-related operations and control logic
    - travelExpenses.cpp
    
    BREAKING CHANGE: project structure has changed

---
+++ functions.h
@@ -0,0 +1,8 @@
+#pragma once
+void displayMenu();
+class Trip {};
+class transportationExpenses {};
+class parkingExpenses {};
+class hotelExpenses {};
+class mealExpenses {};
+class TravelExpenses {};
